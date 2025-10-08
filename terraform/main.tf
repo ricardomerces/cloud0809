978ec8b3c75d7e8ca7bccecb236fc123f8f90aa2
ricardo.merces <ricardo.merces@gmail.com>
2025-10-08 09:38:14 -0300

    update terraform

--- terraform/main.tf
+++ terraform/main.tf
@@ -8,3 +8,14 @@ resource "aws_instance" "web1" {
     Name = "web1"
   }
 }
+
+resource "aws_instance" "web2" {
+  ami           = var.ami_id
+  instance_type = var.instance_type
+  key_name      = "labs"
+  vpc_security_group_ids = [var.security_group_id]
+
+  tags = {
+    Name = "web2"
+  }
+}
--- terraform/output.tf
+++ terraform/output.tf
@@ -1,11 +1,11 @@
 # Saída para o IP Público da instância EC2
-output "ec2_public_ip" {
+output "ec2_public_ip_web1" {
   description = "Endereço IP Público da instância EC2."
   value       = aws_instance.web1.public_ip
 }
 
-# Saída para o FQDN Público da instância EC2
-output "ec2_public_dns" {
-  description = "DNS Público da instância EC2."
-  value       = aws_instance.web1.public_dns
+# Saída para o IP Público da instância EC2
+output "ec2_public_ip_web2" {
+  description = "Endereço IP Público da instância EC2."
+  value       = aws_instance.web2.public_ip
 }
