c178d17580e22ce68a19cd965b077984d8c65118
ricardo.merces <ricardo.merces@gmail.com>
2025-10-08 07:18:33 -0300

    update terraform

---
+++ terraform/main.tf
@@ -0,0 +1,10 @@
+resource "aws_instance" "web1" {
+  ami           = var.ami_id
+  instance_type = var.instance_type
+  key_name      = "labs"
+  vpc_security_group_ids = [var.security_group_id]
+
+  tags = {
+    Name = "web1"
+  }
+}
--- terraform/vars.tf
+++ terraform/vars.tf
@@ -5,19 +5,13 @@ variable "aws_region" {
   default     = "us-east-1"
 }
 
-# Variável para o nome do S3 bucket
-variable "bucket_name" {
-  description = "Nome único para o S3 bucket."
-  type        = string
-  default     = "bucket-cloud0809" # **ATENÇÃO:** O nome deve ser globalmente único
-}
 
 # Variável para o AMI da instância EC2
 variable "ami_id" {
-  description = "ID do AMI (Amazon Machine Image) para a instância EC2 (ex: Amazon Linux 2)."
+  description = "ID do AMI (Ubuntu Server) para a instância EC2."
   type        = string
   # Use um AMI válido para a sua região. Este é um exemplo de Amazon Linux 2 em us-east-1.
-  default     = "ami-052064a798f08f0d3" 
+  default     = "ami-0360c520857e3138f" 
 }
 
 # Variável para o tipo de instância EC2
@@ -27,12 +21,11 @@ variable "instance_type" {
   default     = "t3.micro" 
 }
 
-# Variável para tags (nome)
-variable "default_tags" {
-  description = "Mapa de tags padrão a ser aplicado aos recursos."
-  type        = map(string)
-  default = {
-    Name        = "Terraform-Exemplo"
-    Environment = "Dev"
-  }
+
+# Variável para o ID do Security Group
+variable "security_group_id" {
+  description = "ID do Security Group a ser associado à instância EC2. - SSH"
+  type        = string
+  default     = "sg-0f3309cd5f6127169"
+
 }
